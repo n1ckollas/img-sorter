329440c17a7cea49f4ea44cd6531888d827eb10e
Nick Rebezniuk <nick.rebez@gmail.com>
2020-06-05 21:33:25 -0400

    Distinguishing file from a directory.

--- s.py
+++ s.py
@@ -1,1 +1,9 @@
-    
+import os
+path = "c:\\Users\\n1cko\\Downloads\\"
+for file in os.listdir(path):
+    if os.path.isdir(path + file):
+        print(file + " => is a directory")
+    elif os.path.isfile(path + file):
+        print(file + " => is a file")
+    else:
+        print("It is a special file (socket, FIFO, device file)")
