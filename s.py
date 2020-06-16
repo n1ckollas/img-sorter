761597b0f0467b03b194a4fc02210fad40785c99
Nick Rebezniuk <nick.rebez@gmail.com>
2020-06-16 14:57:55 -0400

    next steps.

--- s.py
+++ s.py
@@ -12,7 +12,7 @@ def is_supported_img(file):
             return True
     return False
 
-def get_date_taken(path):
+def get_img_dates(path):
     f = open(path, 'rb')
     meta_content = exifread.process_file(f) 
     if(meta_content != None):
@@ -24,11 +24,12 @@ def get_date_taken(path):
 
 for file in os.listdir(path):
     if os.path.isdir(path + file):
+        # navigate in directory.
         print(file + " => is a directory")
     elif os.path.isfile(path + file):
         print(file + " => is a file")
         if is_supported_img(file):
-            print(get_date_taken(path + file))
+            print(get_img_dates(path + file))
         else:
             print("NO")
     else:
