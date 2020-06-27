4cbc76b42abaa0d7d59d92c8340fa2731bf6cbad
Nick Rebezniuk <nick.rebez@gmail.com>
2020-06-27 19:41:39 -0400

    Getting image dates.

--- s.py
+++ s.py
@@ -1,37 +1,79 @@
 import os
 import exifread
+from PIL import Image
+import time
 
-path = "D:\\testPics\\"
+path = "D:\\testPics"
+test_file = "336053_4339577334193_1631752716_o.jpg"
 supported_img_formats = ['jpg']
 
 def is_supported_img(file):
-    file_name, ext = os.path.splitext(file)
-    if ext != "" and file_name[0] != '.':
-        ext = str(ext).lower().strip('.')
-        if ext in supported_img_formats:
+    split_file_name = file.rsplit('.')
+    if len(split_file_name) > 0:
+        ext = str(split_file_name[len(split_file_name) - 1]).lower()
+        if ext != "" and split_file_name[0] != '' and ext in supported_img_formats:
             return True
+        else:
+            return False
     return False
 
+
+def string_to_date(date_string):
+    print(date_string.split(" "))
+
+
 def get_img_dates(path):
+    dates = []
+
     f = open(path, 'rb')
     meta_content = exifread.process_file(f) 
-    if(meta_content != None):
+    pill = Image.open(path)._getexif();
+
+    if(pill):
+        print("PIL")
+        for key in pill:
+            if(key == 306):
+                string_to_date(pill[key])
+            elif(key == 36867):
+                string_to_date(pill[key])
+            elif(key == 36868):
+                string_to_date(pill[key])
+            elif(key == 29):
+                string_to_date(pill[key])
+            else:
+                pass
+    elif(meta_content):
+        print("EXIFREAD")
         for key in meta_content:
             if 'date' in str(key).lower():
-                print(key)
-                print(meta_content[key])
+                string_to_date(meta_content[key])
+    else:
+        print("OS.PATH")
+        mtime = os.path.getmtime( path )
+        atime = os.path.getatime( path )
+        ctime = os.path.getctime( path )
+        
+        print(time.ctime(mtime))
+        print(time.ctime(atime))
+        print(time.ctime(ctime))
     return "DONE"
 
-for file in os.listdir(path):
-    if os.path.isdir(path + file):
-        # navigate in directory.
-        print(file + " => is a directory")
-    elif os.path.isfile(path + file):
-        print(file + " => is a file")
-        if is_supported_img(file):
-            print(get_img_dates(path + file))
+def navigate_directories(path):
+    for file in os.listdir(path):
+        if os.path.isdir(path + "\\" +  file):
+            # navigate in directory.
+            print(file + " => is a directory")
+            print("navigating in to the directory => " + file )
+            navigate_directories(path + "\\" + file)
+            
         else:
-            print("NO")
+            if is_supported_img(file):
+                print(file + " => SUPPORTED file")
+                print(get_img_dates(path + "\\" + file))
+            else:
+                print(file + " => NOT Accepted")
     else:
-        print("It is a special file (socket, FIFO, device file)")
+        print("Directory is empty")
+        
 
+navigate_directories(path)
